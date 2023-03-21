6f930fea7c2dd3529d193b66ef27d55c7f5f3e29
smxjqhdak <jhcha98@naver.com>
2023-03-21 13:06:32 +0900

    first commit

---
+++ src/main/java/com/co/kr/SpringExaApplication.java
@@ -0,0 +1,13 @@
+package com.co.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExaApplication.class, args);
+	}
+
+}
